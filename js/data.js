f31b7db9cc884665084c2252e8f9ad93cace3e8f
YehwanSong <song-yehwan@yehwan.local>
2021-05-27 00:16:19 +0900

    add

---
+++ js/data.js
@@ -0,0 +1,26 @@
+var some_array=[
+	[
+		'Hey, have you ever heard <br>\
+		“Now the whole earth had one language and the same words.”?<br>\
+		Do you know what it means?'
+	],[
+		'現在整個地球都用同一個語言,<br>\
+		Nun hatte die ganze Erde eine Sprache und die gleichen Worte.<br>\
+		In which language do you want to know?<br>\
+		Or I misunderstand what you mean…..'
+	],[
+		'heyyyy'
+	],[
+		'<audio controls>\
+		  <source src="sound/sound_01.mp3" type="audio/mpeg">\
+		</audio>'
+	],[
+		'<audio controls>\
+		  <source src="sound/sound_02.mp3" type="audio/mpeg">\
+		</audio>'
+	],[
+		'<img src="img/img1.gif">'
+	],[
+		'<img src="img/img2.jpg">'
+	]
+]
