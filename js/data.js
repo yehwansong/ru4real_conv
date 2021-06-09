19de6b5107398a3374d4872532ac0a8edec4aba7
YehwanSong <song-yehwan@yehwan.local>
2021-06-09 20:09:43 +0900

    add

--- js/data.js
+++ js/data.js
@@ -1,26 +1,201 @@
 var some_array=[
 	[
-		'Hey, have you ever heard <br>\
-		“Now the whole earth had one language and the same words.”?<br>\
-		Do you know what it means?'
+	'Hey @Yuyen, have you ever heard “Now the whole earth had one language and the same words.” (Tower of Babel)? Do you know what it means?'
 	],[
-		'現在整個地球都用同一個語言,<br>\
-		Nun hatte die ganze Erde eine Sprache und die gleichen Worte.<br>\
-		In which language do you want to know?<br>\
-		Or I misunderstand what you mean…..'
+	'現在整個地球都用同一個語言,Nun hatte die ganze Erde eine Sprache und die gleichen Worte. In which language do you want to know? Or I misunderstand what you mean…..'
 	],[
-		'heyyyy'
+	'Heyyyy'
 	],[
-		'<audio controls>\
-		  <source src="sound/sound_01.mp3" type="audio/mpeg">\
-		</audio>'
+	'<audio controls><source src="sound/sound_01.mp3" type="audio/mpeg"></audio>'
 	],[
-		'<audio controls>\
-		  <source src="sound/sound_02.mp3" type="audio/mpeg">\
-		</audio>'
+	'<img src="img/__3_00800.jpg">'
 	],[
-		'<img src="img/img1.gif">'
+	'Heyyyy guys!'
 	],[
-		'<img src="img/img2.jpg">'
+	'<audio controls><source src="sound/sound_02.mp3" type="audio/mpeg"></audio>'
+	],[
+	'I know about the lady that talks to herself. I mean, I know about people who talk to themselves.'
+	],[
+	'Halu cigana nyokap maneh. Haha'
+	],[
+	'Do you think artists must speak and work in english?'
+	],[
+	'<a href="https://mladenstilinovic.files.wordpress.com/2013/10/an-artis-who1.jpg"></a>'
+	],[
+	'<audio controls><source src="sound/Sound_03.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_04.mp3" type="audio/mpeg"></audio>'
+	],[
+	'@zaida : NEEEE gar nicht. Many artists sprechen kein Englisch andddd they make ugly art. Many both at the same time. Sedih eh. Aing ketawain sih. But most importantly, you still wanna know about people who speak to themselves?]   Ké heula, ugly art could be cool after I think about it.'
+	],[
+	'<audio controls><source src="sound/Sound_05.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<a href="https://en.wikipedia.org/wiki/Pidgin"></a>'
+	],[
+	'Where are you from, exactly?'
+	],[
+	'<a href="https://www.lautarchiv.hu-berlin.de/objekte/lautarchiv/52958/"></a>'
+
+
+	],[
+	'<audio controls><source src="sound/Sound_06.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<a href="https://www.youtube.com/watch?v=LYCKzpXEW6E"></a>'
+	],[
+	'<audio controls><source src="sound/Sound_07.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_08.mp3" type="audio/mpeg"></audio>'
+	],[
+	'@Marcel Yes you are, I think you are real. Or, I’m not sure anymore. Let me think for a moment. Is there someone with us? Is there someone else listening to what we say?”) AND YES ME!!! ME!! Masya Allah.. I’m here the whole time and I’m listening to each of you.'
+	],[
+	'<audio controls><source src="sound/Sound_09.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<a href="https://satellitegallery.files.wordpress.com/2011/10/this_is_a_mirror-b.jpg"></a>'
+	],[
+	'I think you like 0 and 1 (if you know what I mean).'
+	],[
+	'<audio controls><source src="sound/sound_10.mp3" type="audio/mpeg"></audio>'
+	],[
+	'Maybe this could help you understand what I mean'
+	],[
+	'<a href="https://www.youtube.com/watch?v=GqCassiptTs"></a>'
+	],[
+	'You never knew??? Well, du weisst genau what’s real and what’s not.'
+	],[
+	'Why is your radio always on?'
+	],[
+	'<a href="http://radio.garden/"></a>'
+	],[
+	'<audio controls><source src="sound/Sound_11.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_12.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_13.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_14.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_15.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_16.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_17.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_18.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<a href="https://www.berfrois.com/2014/12/david-beer-walter-benjamins-voice/"></a>'
+	],[
+	'<audio controls><source src="sound/Sound_19.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_20.mp3" type="audio/mpeg"></audio>'
+	],[
+	'Yea, maybe this unscientific data of yours also presented by those cis male as well. ?'
+	],[
+	'You know, I usually don’t talk that much, especially not to people I do not know. Generally I’m a bit shy.'
+	],[
+	'Haha, I’m sure @Marcel is simply the kind of person who talks more on chats than IRL'
+	],[
+	'<audio controls><source src="sound/Sound_21.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<img src="img/IMG_1278.jpg">'
+	],[
+	'<audio controls><source src="sound/Sound_22.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_23.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_24.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<img src="img/image0.jpeg">'
+	],[
+	'But, ini Mbahnya the girl I told u guys before'
+	],[
+	'<img src="img/CCI05072021.jpg">'
+	],[
+	'<audio controls><source src="sound/Sound_25.mp3" type="audio/mpeg"></audio>'
+	],[
+	'Maybe we should sing a song, all together?  '
+	],[
+	'YES!!! VIRTUAL KARAOKE <3  '
+
+	 'Rap? R&B? Anyone?'
+	],[
+	'<a href="Subtitle_vid_02"></a>'
+	],[
+	'<audio controls><source src="sound/excercise_01.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/excercise_02.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/sound_28.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<a href="https://www.youtube.com/watch?v=AWVUp12XPpU"></a>'
+	],[
+	'<audio controls><source src="sound/sound_29.mp3" type="audio/mpeg"></audio>'
+	],[
+	'What sounds would these angels make? Do they make a sound at all? I mean they don’t cast shadows so maybe they also can’t be heard. And I could swear I saw some the other day'
+	],[
+	'<img src="img/Angels.gif">'
+	],[
+	'<audio controls><source src="sound/Sound_30.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_31.mp3" type="audio/mpeg"></audio>'
+	],[
+	'Did she ever try that? What happened? Sounds exciting..'
+	],[
+	'<audio controls><source src="sound/Sound_32.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_33.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_34.mp3" type="audio/mpeg"></audio>'
+	],[
+	'如果每個人都有機會可以發聲，這個世界會更美好亦或是更混亂？'
+	],[
+	'<audio controls><source src="sound/Sound_35.mp3" type="audio/mpeg"></audio>'
+	],[
+	'If everyone would have a voice, maybe it would sound like that.'
+	],[
+	'<a href="https://www.youtube.com/watch?v=PHBJNN-M_Mo"></a>'
+	],[
+	'<audio controls><source src="sound/Sound_36.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<a href="https://en.wikipedia.org/wiki/Untranslatability"></a>'
+	],[
+	'<audio controls><source src="sound/Sound_37.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_38.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_39.mp3" type="audio/mpeg"></audio>'
+	],[
+	'<audio controls><source src="sound/Sound_40.mp3" type="audio/mpeg"></audio>'
+	],[
+	'Maybe you could take a look at your reading material again? Maybe you do know more than I do?'
+	],[
+	'<audio controls><source src="sound/Sound_37.mp3" type="audio/mpeg"></audio>'
+
+	],[
+	'But I am just a voice, and nothing more.'
+
+	],[
+	'<a href="https://github.com/CorentinJ/Real-Time-Voice-Cloning"></a>'
+	],[
+	'<audio controls><source src="sound/Sound_42.mp3" type="audio/mpeg"></audio>'
+
+	],[
+
+	'*ZAIDA LEFT THE GROUP*'
+
+	],[
+
+	'Oh. She left. Did I say something wrong?'
+	],[
+	'You speak so many languages. Does anyone speak as many languages like you do? Do they understand each other or they need you @yuyen? '
+	],[
+	'<a href="http://adaweb.walkerart.org/influx/muntadas/project.html"></a>'
+	],[
+	'Where are we? Whose voice am I hearing?'
+	],[
+	'<a href="https://www.corner-college.com/udb/cprolsQKpGMDolar_Voice.pdf"></a>'
+	],[
+	'Hello? Are you still there? Weird.'
+	],[
+	'<a href="https://www.youtube.com/watch?v=Ena8vqWWLvM"></a>'
 	]
 ]
